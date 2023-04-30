e24677e572676692b2caf88e627094a20ee6e5a8
faizanali22 <muhammadfaizan.wdc@Outlook.com>
2023-04-30 21:37:46 +0500

    id

--- WishList/Models/Item.cs
+++ WishList/Models/Item.cs
@@ -4,7 +4,7 @@ namespace WishList.Models
 {
     public class Item
     {
-        public int MyProperty { get; set; }
+        public int Id { get; set; }
 
         [Required]
         [MaxLength(50)]
